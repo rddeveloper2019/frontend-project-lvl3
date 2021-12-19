de90ab159d3f50435d687127b674a002d4849ab6
Ridvan Dzhalil <fatzey2010@gmail.com>
2021-12-19 17:40:49 +0300

    7.9

--- src/init.js
+++ src/init.js
@@ -1,4 +1,5 @@
 import controller from './controller';
+import elementCreators from './templates';
 import model from './model';
 
 const initialState = {
@@ -22,9 +23,39 @@ const initialState = {
   autoRefresh: 'on',
 };
 
-const init = (elements) => {
-  const { handlers, utilities } = model(initialState, elements);
-  controller(elements, handlers, utilities);
+const init = () => {
+  document.addEventListener('DOMContentLoaded', () => {
+    const { createBootstrapForm } = elementCreators;
+    const rootFormBlock = document.querySelector('#root-form');
+    const formFeedbackEl = document.querySelector('.feedback');
+
+    const postsContainer = document.querySelector('.posts');
+    const feedsContainer = document.querySelector('.feeds');
+
+    const modal = document.querySelector('#modal');
+    const modalBody = document.querySelector('.modal-body');
+    const modalTitle = document.querySelector('.modal-title');
+    const modalReadMoreLink = document.querySelector('[data-more-link]');
+
+    const { form, input, addBtn } = createBootstrapForm();
+    rootFormBlock.appendChild(form);
+
+    const elements = {
+
+      formContainer: {
+        form, input, addBtn, formFeedbackEl,
+      },
+      modalContainer: {
+        modal, modalBody, modalTitle, modalReadMoreLink,
+      },
+      postsContainer,
+      feedsContainer,
+
+    };
+
+    const { handlers, utilities } = model(initialState, elements);
+    controller(elements, handlers, utilities);
+  });
 };
 
 export default init;
