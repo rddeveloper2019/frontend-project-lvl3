96c01b1d11dd6a13f24bce5b8dd284b8447917e3
Ridvan Dzhalil <fatzey2010@gmail.com>
2021-12-27 20:55:26 +0300

    refactor _5.4 init

--- src/init.js
+++ src/init.js
@@ -2,7 +2,19 @@ import i18next from 'i18next';
 import app from './app';
 import ru from './locales';
 
-const init = () => new Promise((resolve) => {
+// const init = () => new Promise((resolve) => {
+//   const i18n = i18next.createInstance();
+//   i18n.init({
+//     lng: 'ru',
+//     debug: true,
+//     resources: {
+//       ru,
+//     },
+//   });
+//   resolve(i18n);
+// }).then((i18n) => app(i18n)).catch((e) => console.error(e));
+
+const init = () => {
   const i18n = i18next.createInstance();
   i18n.init({
     lng: 'ru',
@@ -11,7 +23,7 @@ const init = () => new Promise((resolve) => {
       ru,
     },
   });
-  resolve(i18n);
-}).then((i18n) => app(i18n)).catch((e) => console.error(e));
+  app(i18n);
+};
 
 export default init;
